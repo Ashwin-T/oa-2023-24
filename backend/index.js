1d4675e9cc3f97a404c11a68235266ba809b62fe
Ashwin Talwalkar <atalwalkar719@gmail.com>
2023-09-17 23:20:39 -0500

    hosting

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,49 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import axios from "axios";
 
-const app = express()
+const app = express();
 
-app.use(cors({
-    origin: "http://localhost:5500"
-}))
+// Use the cors middleware to enable CORS
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send({
-        x: [1, 2, 3],
-        y: [4, 5, 6]
-    }).status(200)
-})
+app.get('/', async (req, res) => {
+    try {
+        const url = 'https://api.madgrades.com/v1/courses/e449f054-a344-3556-b18a-5333e51ceb82/grades';
+        const token = 'e468f2c9c97747a3912989978d211f2a';
 
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': `Token token=${token}`
+            },
+        });
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`server listening on port ${port} 🚀`))
+        if (response.status === 200) {
+            const data = response.data;
+            const aCountPerTerm = [];
+            const totalPeoplePerTerm = [];
+            // Iterate through the courseOfferings for each term
+            data.courseOfferings.forEach((term) => {
+                // Extract the "aCount" value for the term
+                const aCount = term.cumulative.aCount;
+                const total = term.cumulative.total;
+                // Push the "aCount" value to the array
+                aCountPerTerm.push(aCount);
+                totalPeoplePerTerm.push(total);
+            });
+            
+            // Now, aCountPerTerm contains an array of "aCount" values per term
+            res.json({
+                "aCountPerTerm": aCountPerTerm,
+                "totalPeoplePerTerm": totalPeoplePerTerm
+            });
+        } else {
+            throw new Error('Request failed');
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`server listening on port ${port} 🚀`));
--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,59 @@
 const graphDiv = document.getElementById("graph");
 
 fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
-).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
-})
+    "http://localhost:3000/",
+    {
+        method: "GET",
+        headers: {
+            "Origin": "http://localhost:5500"
+        }
+    }
+)
+.then(async res => {
+    if (res.ok) {
+        const data = await res.json();
 
+        // Extract data from the response
+        const aCountPerTerm = data.aCountPerTerm;
+        const totalPeoplePerTerm = data.totalPeoplePerTerm;
+
+        // Generate years since 2018 as x-axis values
+        const yearsSince2018 = Array.from({ length: aCountPerTerm.length }, (_, i) => 0 + i);
+
+        // Create traces for the two graphs
+        const aCountTrace = {
+            x: yearsSince2018,
+            y: aCountPerTerm,
+            type: 'scatter',
+            name: 'A Count of CS 400'
+        };
+
+        const totalPeopleTrace = {
+            x: yearsSince2018,
+            y: totalPeoplePerTerm,
+            type: 'scatter',
+            name: 'Total People Taking CS 400'
+        };
+
+        // Create a layout for the graphs
+        const layout = {
+            title: 'The Number of A\'s in CS 400 Over Time',
+            xaxis: {
+                title: 'Semester since Spring 2018'
+            },
+            yaxis: {
+                title: 'Values'
+            }
+        };
+
+        // Create the Plotly graph with the traces and layout
+        Plotly.newPlot(graphDiv, [aCountTrace, totalPeopleTrace], layout);
+    } else {
+        console.error("Failed to fetch data:", res.statusText);
+        // Handle the error as needed
+    }
+})
+.catch(error => {
+    console.error("Error while fetching data:", error);
+    // Handle the error as needed
+});
